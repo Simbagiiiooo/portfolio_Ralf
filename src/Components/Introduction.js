92b403444e3bbb03adf75552189091792ed0a98a
vanEckWebdesign <ralf22121973@gmail.com>
2022-01-18 11:39:07 +0100

    modified Footer small devises

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -5,19 +5,19 @@ const Introduction=()=> {
     return (
         <div className="introduction">
             <p>Graag wil ik mezelf aan u voorstellen. <br></br><br></br>Mijn naam is Ralf van Eck.<br></br><br></br>
-                Het is al een hele tijd geleden dat ik gesolliciteerd op een functie die een hele grote verandering en impact op mijn leven gaat zijn.<br></br>
-                Nu heb ik op 47 jarige de Front-end development cursus van Winc Academy gedaan en een certificaat behaald.
-                Ik dacht dat met dit certificaat snel een droombaan te vinden, maar…<br></br><br></br>
-                Dat valt dus tegen!!<br></br><br></br>
+                Ik heb op 47 jarige de Front-end development cursus van Winc Academy gedaan en een certificaat behaald.<br></br>
+                Het was soms best pittig en het kostte soms iets meer moeite dan gedacht maar het hele proces vanaf de eerste stap (HTML) tot en met de eindopdracht was enorm uitdagend en leuk.<br></br>
+                Creatief bezig zijn, het zoeken naar de juiste oplossing en doorzetten... dat zijn de dingen waar ik energie uithaal en waar ik zeker in door wil gaan.<br></br>
+                Ik wil met dit certificaat een droombaan te vinden, maar…<br></br><br></br>
                 Ik ben tot de conclusie gekomen dat mijn sollicitatie capaciteiten nog uit 1997 stammen.
                 Ik ben nu bezig met de cursus Personal Career Branding en inderdaad zijn mijn sollicitatie skills vastgeroest.
                 Ik ben bezig mijn sollicitatie capaciteiten naar deze tijd te transformeren.<br></br><br></br>
                 Ik probeer in mijn vrije tijd mijn programmeer skills te verbeteren.
-                Maar omdat ik aan het werk ben als logistiek medewerker vind ik dat ik daar te weinig in investeer. 
+                Maar omdat ik aan het werk ben als logistiek medewerker vind ik dat ik daar te weinig in kan investeren. 
                 Ik wil graag fulltime mijn skills verbeteren.<br></br><br></br>
                 Ik ben een sociale, enthousiaste en behulpzame (jonge)man die al wat levenservaring met zich meebrengt.<br></br><br></br>
                 Op deze pagina heb ik projecten die ik maakte in het kader van mijn opleiding.<br></br>
-                Ik ben mijn portfolio verder uit aan het ontwikkelen door projecten in mijn vrije tijd te ontwikkelen.</p>
+                Ik ben mijn portfolio verder aan het ontwikkelen door projecten in mijn vrije tijd te bouwen.</p>
             <img src = {portrait} alt = "portret Ralf van Eck"  ></img>
         </div>
     )
