d065836246d4c8d5acc0f2b9516d4a5a4e66215a
vanEckWebdesign <ralf22121973@gmail.com>
2022-01-18 11:22:53 +0100

    modified introduction

--- src/Components/Introduction.js
+++ src/Components/Introduction.js
@@ -4,24 +4,20 @@ import portrait from './img/profielfoto.jpg'
 const Introduction=()=> {
     return (
         <div className="introduction">
-            <p>Mijn naam is Ralf van Eck en ben 48 jaar. <br></br>
-            Ik heb al veel verschillende banen gehad, maar dit zijn naar mij mening altijd banen onder mijn niveau geweest.
-            Dit is volgens mij te wijten geweest omdat ik niet wist wat ik wilde worden.
-            Ik heb 2 jaar geleden een aantal persoonlijkheid en beroepskeuze testen gedaan.
-            Daar zij een aantal zaken naar boven gekomen namelijk:<br></br><br></br>
-            - Computers<br></br>
-            - Ontwerpen<br></br>
-            - Ontwikkeling(maken en/of bouwen)<br></br>
-            - Analytisch<br></br>
-            - Onderzoekend<br></br><br></br>
-            Daar ben ik mee aan de slag gegaan en vanaf september 2020 tot september 2021 ben ik bezig geweest met de opleiding Front-end development bij Winc
-            Academy. De opleiding heb ik inmiddels afgerond.<br></br>
-            Ik heb de opleiding met veel plezier gedaan. Omdat je bij de opleiding veel zelf bezig bent en veel zelf uit moet zoeken. 
-            Soms is het niet makkelijk geweest en daardoor veel over en van mezelf geleerd.<br></br>
-            Ik ben me ervan bewust dat ik een junior ben en nog veel moet leren.<br></br><br></br>
-            Ik ben ervan overtuigd dat ik ondanks mijn leeftijd met de juiste begeleiding een waardevolle Developer kan worden!<br></br><br></br>
-            Op deze pagina heb ik projecten die ik maakte in het kader van mijn opleiding maar ook andere projecten.
-            </p>
+            <p>Graag wil ik mezelf aan u voorstellen. <br></br><br></br>Mijn naam is Ralf van Eck.<br></br><br></br>
+                Het is al een hele tijd geleden dat ik gesolliciteerd op een functie die een hele grote verandering en impact op mijn leven gaat zijn.<br></br>
+                Nu heb ik op 47 jarige de Front-end development cursus van Winc Academy gedaan en een certificaat behaald.
+                Ik dacht dat met dit certificaat snel een droombaan te vinden, maar…<br></br><br></br>
+                Dat valt dus tegen!!<br></br><br></br>
+                Ik ben tot de conclusie gekomen dat mijn sollicitatie capaciteiten nog uit 1997 stammen.
+                Ik ben nu bezig met de cursus Personal Career Branding en inderdaad zijn mijn sollicitatie skills vastgeroest.
+                Ik ben bezig mijn sollicitatie capaciteiten naar deze tijd te transformeren.<br></br><br></br>
+                Ik probeer in mijn vrije tijd mijn programmeer skills te verbeteren.
+                Maar omdat ik aan het werk ben als logistiek medewerker vind ik dat ik daar te weinig in investeer. 
+                Ik wil graag fulltime mijn skills verbeteren.<br></br><br></br>
+                Ik ben een sociale, enthousiaste en behulpzame (jonge)man die al wat levenservaring met zich meebrengt.<br></br><br></br>
+                Op deze pagina heb ik projecten die ik maakte in het kader van mijn opleiding.<br></br>
+                Ik ben mijn portfolio verder uit aan het ontwikkelen door projecten in mijn vrije tijd te ontwikkelen.</p>
             <img src = {portrait} alt = "portret Ralf van Eck"  ></img>
         </div>
     )
